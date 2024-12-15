27c390d13d03b9ca4fa88679263be13fb8e14988
Yaowen Mei <ywmei@google.com>
2024-12-15 15:42:29 +0000

    Long Pulling API

--- cmd/my-app/main.go
+++ cmd/my-app/main.go
@@ -9,13 +9,15 @@ import (
 )
 
 // Start the server with: go run main.go
-// curl -X PUT -d "hello" http://localhost:8080/put
-// curl http://localhost:8080/get
-// curl "http://localhost:8080/search?q=hello"
+// curl -i -w "\nExit Code: %{http_code} " -X PUT -d "hello" http://localhost:8080/put
+// curl -i -w "\nExit Code: %{http_code} " http://localhost:8080/get
+// curl -i -w "\nExit Code: %{http_code} " http://localhost:8080/get/longpoll
+// curl -i -w "\nExit Code: %{http_code} " "http://localhost:8080/search?q=hello"
 func main() {
 	http.HandleFunc("/put", handlers.PutHandler)
 	http.HandleFunc("/get", handlers.GetHandler)
 	http.HandleFunc("/search", handlers.SearchHandler)
+	http.HandleFunc("/get/longpoll", handlers.LongPollHandler)
 
 	fmt.Println("Server listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
